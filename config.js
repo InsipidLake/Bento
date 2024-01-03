18cb15fb09fe8fbd7e8c2dfa7b94645c1d6329b3
Saketh-byte <Saketh1121@gmail.com>
2024-01-03 16:05:19 +0530

    changed theme to Nord

--- config.js
+++ config.js
@@ -27,7 +27,7 @@ const CONFIG = {
 
 	// Weather
 	weatherKey: 'de67ef3bdb5c88771edba8ea2bb38649', // Write here your API Key
-	weatherIcons: 'Onedark', // 'Onedark', 'Nord', 'Dark', 'White'
+	weatherIcons: 'Nord', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
 
